a1ec2e6fc3e4623a8642f68a2d8975028c1a463b
miguelzvd <dilsonmiguel25@gmail.com>
2024-01-27 14:11:30 -0300

    Update CSS styles and JavaScript functionality

--- api-artists/middlewares.js
+++
@@ -1,8 +0,0 @@
-// middlewares.js
-module.exports = (req, res, next) => {
-  if (req.method === "GET" && req.path === "/artists" && req.query.name_like) {
-    const searchTerm = req.query.name_like.toLowerCase();
-    req.query.name_like = searchTerm; // Convertendo o termo de pesquisa para minúsculas
-  }
-  next();
-};
--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-const searchInput = document.getElementById('search-input');
+const searchInput = document.getElementById("search-input");
 const resultArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById('result-playlists');
+const resultPlaylist = document.getElementById("result-playlists");
+const closeButton = document.getElementById("close-button");
+const footer = document.getElementById("footer");
+const search_area = document.getElementById("search-area");
 
 async function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
-    
-    try {
-        const response = await fetch(url);
-        const result = await response.json();
-        displayResults(result);
-        console.log(result); // Aqui você pode acessar o resultado da requisição
-        return result; // Se desejar acessar o resultado fora dessa função
-    } catch (error) {
-        console.error('Erro ao fazer a solicitação à API:', error);
-        // Trate o erro aqui, se necessário
-    }
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    displayResults(result);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error("Erro ao fazer a solicitação à API:", error);
+  }
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+  resultPlaylist.classList.add("hidden");
+  const artistName = document.getElementById("artist-name");
+  const artistImage = document.getElementById("artist-img");
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
+  result.forEach((element) => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
 
-    resultArtist.classList.remove('hidden');
+  resultArtist.classList.remove("hidden");
 }
 
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase(); // Converter para minúsculas
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
-    }
-    console.log(searchTerm);
-    requestApi(searchTerm);
-})
+document.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultPlaylist.classList.remove("hidden");
+    resultArtist.classList.add("hidden");
+    return;
+  }
+  console.log(searchTerm);
+  requestApi(searchTerm);
+});
 
+closeButton.addEventListener("click", function () {
+  footer.classList.add("hidden");
+});
+
+search_area.addEventListener("click", function () {
+  searchInput.focus();
+});
